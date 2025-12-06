0a4667bb441757031e615788afac9dce24f5536a
rdtbra <reinaldo@adtinformatica.com.br>
2025-12-06 12:37:00 -0300

    cursos

---
+++ categorias/cursos/assets/CURgroups.js
@@ -0,0 +1,72 @@
+// ============================================================================
+// CURgroups.js
+// ============================================================================
+
+window.GROUPS = [
+  {
+    "id": "cur-01-pc",
+    "name": "PLACEHOLDER",
+    "color": "#4f46e5",
+    "icon": "assets/EMT-01-ES.png",
+    "iconHref": "https://drive.google.com/open?id=1q54w5ZOAwk6af8_414Psuz8Cre81wR2m&usp=drive_fs",
+    "collapsed": true,
+    "items": [
+      {
+        "code": "M01",
+        "label": "Claude 4 Sonnet",
+        "provider": "AdaptaONE",
+        "url": "https://app.adapta.one/chats/86a63d0c-6595-4549-9dd7-669dddd30ed5",
+        "checked": true,
+        "img": ""
+      },
+      {
+        "code": "M02",
+        "label": "Gemini 2.5 Flash",
+        "provider": "AdaptaONE",
+        "url": "https://app.adapta.one/chats/a978446e-2d12-45d2-87dc-9e45a2bbea01",
+        "checked": true,
+        "img": ""
+      },
+      {
+        "code": "M03",
+        "label": "GPT-4o",
+        "provider": "openAI",
+        "url": "https://chatgpt.com/g/g-p-68abc6e4e8508191b025b4e541779c61/c/6906449f-bb84-8332-9ef6-ec3e4c8325b4",
+        "checked": true,
+        "img": ""
+      },
+      {
+        "code": "M04",
+        "label": "Perplexity",
+        "provider": "AdaptaONE",
+        "url": "https://app.adapta.one/chats/70088b16-aaff-4bc6-b6c7-6c91735274ab",
+        "checked": true,
+        "img": ""
+      },
+      {
+        "code": "M05",
+        "label": "Sabia 3.1",
+        "provider": "Inner.AI",
+        "url": "https://app.innerai.com/projects/5ab190c0-7035-46d4-8f06-450974e38bad?sessionId=69359d82-f860-414b-91f7-a79284ea3a80",
+        "checked": true,
+        "img": ""
+      },
+      {
+        "code": "SUP",
+        "label": "GPT-5",
+        "provider": "openAI",
+        "url": "https://chatgpt.com/g/g-p-68abc6e4e8508191b025b4e541779c61/c/68c7648c-6638-8004-a3d5-6f1d7e2b0cf6",
+        "checked": true,
+        "img": ""
+      },
+      {
+        "code": "REV",
+        "label": "Command R",
+        "provider": "Inner.AI",
+        "url": "https://app.innerai.com/projects/5ab190c0-7035-46d4-8f06-450974e38bad?sessionId=65ae6d64-cdf7-43fa-8aee-bf6938dd1f89",
+        "checked": true,
+        "img": ""
+      }
+    ]
+  }
+];
