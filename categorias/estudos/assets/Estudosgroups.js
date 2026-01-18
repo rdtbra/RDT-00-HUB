0932af7d007b18b0fc9dc85855f399ce970ec5e8
rdtbra <reinaldo@adtinformatica.com.br>
2026-01-17 22:13:51 -0300

    Nome do icone

--- categorias/estudos/assets/Estudosgroups.js
+++ categorias/estudos/assets/Estudosgroups.js
@@ -3026,7 +3026,7 @@ window.GROUPS = [
     "id": "emt-79-ee",
     "name": "EMT-79-EE - Malvino&Outros - Electronic Principles 9ed",
     "color": "#8b86ff",
-    "icon": "assets/EM-79-EE.png",
+    "icon": "assets/EMT-79-EE.png",
     "iconHref": "https://drive.google.com/open?id=1RHlp8M5i4YuRrEqnIVkMcIJ4gIWiRV1V&usp=drive_fs",
     "items": [
       {
